8d5ec1e2085a5070aa44ff9ba711c13956b74275
yeonggwangchoi <dudrhkd7811@naver.com>
2023-04-24 16:50:48 +0900

    서브 pc 소스코드 수정

--- Face_Control_Wheelchair_subpc/main.py
+++ Face_Control_Wheelchair_subpc/main.py
@@ -6,7 +6,7 @@ import numpy as np
 #고정상수&고정변수 초기화
 EPOCH = 500000
 atmega_port = 'com15'
-raspberry_port = 'com15'
+raspberry_port = 'com18'
 red_traffic = False
     
 if __name__ == "__main__":
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     ser_at = atmega.ATmega128()
     com_at = ser_at.init(port = atmega_port, baudrate = 9600)
     ser_rasp = raspberry.Raspberry()
-    com_desk = ser_rasp.init(port = atmega_port, baudrate = 9600)
+    com_desk = ser_rasp.init(port = raspberry_port, baudrate = 9600)
     cam = cam.libcamera()
     ch0, ch1 = cam.initial_setting(capnum=2)
 
@@ -22,10 +22,10 @@ if __name__ == "__main__":
         _, frame0, _, frame1 = cam.camera_read(ch0, ch1)
 
         motor = com_desk.read()
-
+    
         if np.any(cam.object_detect_cls == 9) == True:
-           red_traffic = cam.traffic_light_detect(frame1)
-        obstacle = cam.object_detect(frame1)
+           red_traffic = cam.traffic_light_detect(frame0)
+        obstacle = cam.object_detect(frame0)
 
         if (red_traffic == True and motor == b'G') or (obstacle == False and motor == b'G'):
             com_at.write(b'S')
